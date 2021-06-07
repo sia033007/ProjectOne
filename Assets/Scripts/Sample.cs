ee977b6b8be27912164feb09a3f42dcf74a0f1cf
amin khosravi <siavashmehmandoost@gmail.com>
2021-06-07 22:44:58 +0430

    1stCommit

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator LoadAssets(string url)
+    {
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        yield return request.SendWebRequest();
+
+        if(request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+            var prefab = bundle.LoadAsset("Player");
+            GameObject player = Instantiate(prefab) as GameObject;
+        }
+    }
+}
